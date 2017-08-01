6a1f6de8b1ff791808ab8eea65d48981afdbadb3
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2017-08-01 20:46:57 +0200

    Added extensions for Keyed Hash Algorithms to/from JWK

---
+++ src/THNETII.Security.JOSE/JsonWebOctExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using static THNETII.Security.JOSE.JsonWebAlgorithm;
+
+namespace THNETII.Security.JOSE
+{
+    public static class JsonWebOctExtensions
+    {
+        public static JsonOctWebKey ExportJsonWebKey(this KeyedHashAlgorithm hash)
+        {
+            if (hash == null)
+                throw new ArgumentNullException(nameof(hash));
+
+            return new JsonOctWebKey { Key = hash.Key };
+        }
+
+        public static void ImportJsonWebKey(this KeyedHashAlgorithm hash, JsonOctWebKey jwk)
+        {
+            if (hash == null)
+                throw new ArgumentNullException(nameof(hash));
+            else if (jwk == null)
+                throw new ArgumentNullException(nameof(jwk));
+
+            hash.Key = jwk.Key;
+        }
+    }
+}
