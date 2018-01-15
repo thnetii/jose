971e1a50aa234046c23ae40283f55ebfe8fe41df
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2018-01-15 09:02:41 +0100

    Initial Draft of common HTTP utility extension library

---
+++ src/THNETII.Common.Http/HttpContentExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Http;
+
+namespace THNETII.Common.Http
+{
+    public static class HttpContentExtensions
+    {
+        public static bool IsHtml(this HttpContent httpContent, bool trueIfNoMediaType = true)
+        {
+            if (httpContent == null)
+                return false;
+            return httpContent.Headers.ContentType.IsHtml(trueIfNoMediaType);
+        }
+    }
+}
